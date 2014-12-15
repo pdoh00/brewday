c943a82c742fc65d3b97f9297580d6e4f269dbde
Phil <pdoh00@gmail.com>
2014-12-15 13:22:13 -0800

    updated android app Login screen

--- src/brewday.android/brewday.android/LoginActivity.cs
+++ src/brewday.android/brewday.android/LoginActivity.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.IO;
 using System.Threading.Tasks;
 using Android.App;
 using Android.Content;
@@ -37,15 +39,50 @@ namespace brewday.android
 				prgDlg.SetMessage("Logging in. Please wait...");
 				prgDlg.SetCancelable(false);
 				prgDlg.Show();
-				await Login();
+
+				var emailEditText = FindViewById<EditText>(Resource.Id.EmailEditText);
+				var passwordEditText = FindViewById<EditText>(Resource.Id.PasswordEditText);
+
+				await Login(emailEditText.Text, passwordEditText.Text);
 				prgDlg.Dismiss();
 			};
 
 		}
 
-		async Task Login(){
-			var emailEditText = FindViewById<Button>(Resource.Id.EmailEditText);
-			var passwordEditText = FindViewById<Button>(Resource.Id.PasswordEditText);
+		async Task Login(string email, string password){
+
+
+//			HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://localhost:44303/login");
+//
+//			req.Method = "POST";
+//			req.ContentType = "application/x-www-form-urlencoded";         
+//			req.ContentLength = 6;
+//			var strRequest = string.Format ("grant_type=password&username={0}&password={1}", email, password);
+//			var streamOut = new StreamWriter(req.GetRequestStream(), System.Text.Encoding.ASCII);
+//			streamOut.Write(strRequest);
+//			streamOut.Close();
+//			var streamIn = new StreamReader(req.GetResponse().GetResponseStream());
+//			string strResponse = streamIn.ReadToEnd();
+//			streamIn.Close();
+//
+//			req.BeginGetResponse ((ar) => {
+//				var request = (HttpWebRequest)ar.AsyncState;
+//				using (var response = (HttpWebResponse)request.EndGetResponse (ar)){ 
+//					var s = response.GetResponseStream();
+//					Console.WriteLine(s);
+//				}
+//			}, req);
+
+//			if (result.IsSuccessStatusCode) {
+//				//TODO: get token. store local
+//				var token = await result.Content.ReadAsStringAsync ();
+//				StartActivity (typeof(MainActivity));
+//			} else {
+//				var toast = new Toast (this);
+//				toast.SetText ("Login failed.");
+//				toast.Duration = ToastLength.Long;
+//				toast.Show ();
+//			}
 
 			using (var httpClient = new HttpClient ()) {
 				httpClient.BaseAddress = new Uri ("https://localhost:44303/");
@@ -58,8 +95,8 @@ namespace brewday.android
 					var content = new FormUrlEncodedContent(new[] 
 						{
 							new KeyValuePair<string, string>("grant_type", "password"),
-							new KeyValuePair<string, string>("username", emailEditText.Text),
-							new KeyValuePair<string, string>("password", passwordEditText.Text),
+							new KeyValuePair<string, string>("username", email),
+							new KeyValuePair<string, string>("password", password),
 						});
 					result = await httpClient.PostAsync("login", content);
 				}
